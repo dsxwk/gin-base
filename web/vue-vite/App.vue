5a5bbf5791ec85d9650c0fe58d5c8ccc126940f7
dsx <25076778@qq.com>
2024-07-26 14:27:06 +0800

    菜单切换调整

--- web/vue-vite/App.vue
+++ web/vue-vite/App.vue
@@ -1,35 +1,3 @@
-<script setup>
-import {onMounted} from "vue";
-import $ from 'jquery';
-import {Functions} from "@utils/functions/functions";
-document.title = '后台管理';
-
-let funcs = new Functions();
-
-onMounted(async function () {
-  setTimeout(updateMenuActive, 100);
-});
-
-// 更新菜单选中
-function updateMenuActive() {
-  $('body a').each(function () {
-    //设置所有连接带语言
-    $(this).attr('href', funcs.setUrlParam($(this).attr('href'), 'lang', funcs.getLang()));
-  });
-
-  let sidebarMenu = $('#SidebarMenu');
-  let url = window.location;
-  url = url.pathname + url.hash;
-  if (url.indexOf('?') == -1) {
-    url = url + '?lang=' + funcs.getLang();
-  }
-
-  sidebarMenu.find('a[href="' + url + '"]').addClass('active');
-  sidebarMenu.find('a[href="' + url + '"]').parent('li').parent().parent().addClass('menu-open');
-  sidebarMenu.find('a[href="' + url + '"]').parent().parent().prev().addClass('active');
-}
-</script>
-
 <template>
   <router-view></router-view>
 </template>
--- web/vue-vite/views/layouts/sidebar.vue
+++ web/vue-vite/views/layouts/sidebar.vue
@@ -1,7 +1,49 @@
 <script setup>
 import {Functions} from '@utils/functions/functions';
+import {onMounted, nextTick, ref} from "vue";
 
 let funcs = new Functions();
+
+// 菜单
+const menu = ref([
+  {
+    name: funcs.lang('Dashboard'), uri: 'javascript:void(0);', icon: 'fas fa-tachometer-alt', path:'',
+    children: [
+      {name: funcs.lang('Dashboard') + ' v1', uri: '/', icon: 'far fa-circle', path:''},
+    ]
+  },
+  {
+    name: funcs.lang('User Manage'), uri: 'javascript:void(0);', icon: 'far fa-circle', path:'',
+    children: [
+      {name: funcs.lang('User List'), uri: '/user', icon: 'far fa-circle', path:''},
+    ]
+  }
+]);
+
+onMounted(async () => {
+  await nextTick();
+  updateMenuActive();
+});
+
+// 更新菜单选中
+async function updateMenuActive() {
+  $('body a').each(function () {
+    //设置所有连接带语言
+    $(this).attr('href', funcs.setUrlParam($(this).attr('href'), 'lang', funcs.getLang()));
+  });
+
+  let sidebarMenu = $('#SidebarMenu');
+  console.log(sidebarMenu);
+  let url = window.location;
+  url = url.pathname + url.hash;
+  if (url.indexOf('?') == -1) {
+    url = url + '?lang=' + funcs.getLang();
+  }
+
+  sidebarMenu.find('a[href="' + url + '"]').addClass('active');
+  sidebarMenu.find('a[href="' + url + '"]').parent('li').parent().parent().addClass('menu-open');
+  sidebarMenu.find('a[href="' + url + '"]').parent().parent().prev().addClass('active');
+}
 </script>
 <template>
   <!-- Main Sidebar Container -->
@@ -31,36 +73,19 @@ let funcs = new Functions();
         <ul id="SidebarMenu" class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item">
-            <a href="javascript:void(0);" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+          <li v-for="(item, key) in menu" class="nav-item">
+            <a :href="item.uri" class="nav-link">
+              <i class="nav-icon" :class="item.icon"></i>
               <p>
-                {{funcs.lang('Dashboard')}}
+                {{item.name}}
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="/" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>{{funcs.lang('Dashboard')}} v1</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a href="javascript:void(0);" class="nav-link">
-              <i class="nav-icon far fa-circle"></i>
-              <p>
-                {{funcs.lang('User Manage')}}
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="/user" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>{{funcs.lang('User List')}}</p>
+              <li v-for="(i, k) in item.children" class="nav-item">
+                <a :href="i.uri" class="nav-link">
+                  <i class="nav-icon" :class="i.icon"></i>
+                  <p>{{i.name}}</p>
                 </a>
               </li>
             </ul>

97a26abf61adf62309f3e18e45ed1bf64c4a71f1
dsx <25076778@qq.com>
2024-08-06 10:23:07 +0800

    调整路由

--- web/vue-vite/routers/router.js
+++ web/vue-vite/routers/router.js
@@ -1,29 +1,22 @@
-// 导入需要的组件
-import Index from '@views/layouts/index.vue';
-import Login from '@views/login/login.vue';
-import UserIndex from '@views/user/index.vue';
-
-import NotFound from '@views/errPage/404.vue'
-
 // 定义路由配置
 const routers = [
     {
         path: '/login',
-        component: Login,
+        component: () => import('@views/login/login.vue'),
         meta: {
             title: '登录'
         }
     },
     { 
         path: '/', 
-        component: Index,
+        component: () => import('@views/layouts/index.vue'),
         meta: { 
             title: '首页' 
         } 
     },
     {
         path: '/user',
-        component: UserIndex,
+        component: () => import('@views/user/index.vue'),
         meta: {
             title: '用户列表'
         }
@@ -32,7 +25,7 @@ const routers = [
     // 可以继续添加其他路由配置
     { 
         path: '/:catchAll(.*)', 
-        component: NotFound,
+        component: import('@views/errPage/404.vue'),
         meta: { 
             title: '404 Not Found' 
         } 

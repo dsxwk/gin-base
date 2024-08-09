b07cf3cdd4d476aab529f243215a54a9cee1adf8
dsx <25076778@qq.com>
2024-08-09 15:36:38 +0800

    调整动态路由页面显示问题

--- web/vue-vite/layouts/main/index.vue
+++ web/vue-vite/layouts/main/index.vue
@@ -2,30 +2,20 @@
   <Breadcrumb/>
   <Tab/>
   <el-main>
-    <transition appear name="fade-transform" mode="out-in">
-      <keep-alive :include="keepAliveName">
-        <router-view :path="route.fullPath"/>
+    <router-view v-slot="{Component, route}">
+      <keep-alive>
+        <component :is="Component" :key="route.fullPath"/>
       </keep-alive>
-    </transition>
+    </router-view>
   </el-main>
   <el-footer>
     <Footer/>
   </el-footer>
 </template>
 <script setup>
-import {ref} from 'vue';
-import {useRoute} from 'vue-router';
 import Breadcrumb from '@/layouts/main/breadcrumb/index.vue';
 import Tab from '@/layouts/main/tab/index.vue';
 import Footer from '@/layouts/footer/index.vue';
-import Home from '@/views/home/index.vue';
-import UserIndex from '@/views/user/index.vue';
-const route = useRoute();
-
-const keepAliveName = ref([
-  Home,
-  UserIndex
-]);
 </script>
 <style lang="scss" scoped>
 @import './index.scss';

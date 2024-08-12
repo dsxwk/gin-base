329fb96303f3b3176a2c442545f763d74f5709dd
dsx <25076778@qq.com>
2024-08-12 11:33:29 +0800

    调整tab动态选中以及关闭

--- web/vue-vite/layouts/main/tab/index.vue
+++ web/vue-vite/layouts/main/tab/index.vue
@@ -2,7 +2,7 @@
   <div class="tabs-box">
     <div class="tabs-menu">
       <el-tabs v-model="tabsMenuValue" type="card" @tab-click="tabClick" @tab-remove="tabRemove">
-        <el-tab-pane v-for="item in tabsMenuList" :key="item.path" :label="item.title" :name="item.path" :closable="item.close">
+        <el-tab-pane v-for="item in tabsMenuList" :key="item.path" :label="item.title" :name="item.path" :closable="item.close && item.path !== HOME_URL">
           <template #label>
             <el-icon v-if="item.icon && tabsIcon" class="tabs-icon">
               <component :is="item.icon"></component>
@@ -45,14 +45,15 @@
   </div>
 </template>
 <script setup>
-import {ref} from 'vue';
+import { ref, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
+import {HOME_URL} from '@/config/configs.js';
 
 const route = useRoute();
 const router = useRouter();
 const tabsMenuValue = ref(route.fullPath);
 const tabsIcon = ref(true);
-const tabsMenuList = ref([
+/*const tabsMenuList = ref([
   {
     "icon": "HomeFilled",
     "title": "首页",
@@ -69,13 +70,38 @@ const tabsMenuList = ref([
     "close": true,
     "isKeepAlive": true
   }
-]);
+]);*/
+const tabsMenuList = ref([]);
+
+watch(route, (newRoute) => {
+  const exists = tabsMenuList.value.some(tab => tab.path === newRoute.fullPath);
+  if (!exists) {
+    tabsMenuList.value.push({
+      title: newRoute.meta.title || newRoute.name,
+      path: newRoute.fullPath,
+      close: newRoute.meta.close || true,
+      icon: newRoute.meta.icon || ''
+    });
+  }
+  tabsMenuValue.value = newRoute.fullPath;
+}, { immediate: true });
 const tabClick = (tabItem) => {
   const fullPath = tabItem.props.name;
   router.push(fullPath);
 };
-const tabRemove = () => {
-  
+const tabRemove = (removedPath) => {
+  // 查找被关闭的标签页在 tabsMenuList 中的位置
+  const index = tabsMenuList.value.findIndex(tab => tab.path === removedPath);
+  if (index !== -1) {
+    // 从 tabsMenuList 中移除被关闭的标签页
+    tabsMenuList.value.splice(index, 1);
+  }
+  // 如果被关闭的标签页是当前激活的标签页
+  if (tabsMenuValue.value === removedPath && tabsMenuList.value.length > 0) {
+    // 切换到下一个标签页，如果有的话
+    const nextTab = tabsMenuList.value[Math.max(0, index - 1)];
+    router.push(nextTab.path);
+  }
 }
 const refresh = () => {
 

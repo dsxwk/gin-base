8ea49a15f8a25dd1713a8f31dbcaf22b573cb7c9
dsx <25076778@qq.com>
2024-09-20 11:56:54 +0800

    调整后台主题样式

--- web/vue-vite/src/main.js
+++ web/vue-vite/src/main.js
@@ -1,20 +1,30 @@
 import { createApp } from 'vue';
 import App from '../App.vue';
+import {ref} from 'vue';
 import Functions from '@/utils/functions';
 import {LOGIN_URL} from '@/config';
 import TablePlus from 'icourt-table';
+import {getDarkColor, getLightColor} from '@/utils/color';
+const DEFAULT_PRIMARY = '#009688';
+const isDark = ref(false);
+// global scss
+document.documentElement.style.setProperty('--el-color-primary', DEFAULT_PRIMARY);
+for (let i = 1; i <= 9; i++) {
+    const primaryColor = isDark.value ? `${getDarkColor(DEFAULT_PRIMARY, i / 10)}` : `${getLightColor(DEFAULT_PRIMARY, i / 10)}`;
+    document.documentElement.style.setProperty(`--el-color-primary-light-${i}`, primaryColor);
+}
 // reset style sheet
-import "@/styles/reset.scss";
+import '@/styles/reset.scss';
 // CSS common style sheet
 import "@/styles/common.scss";
 // iconfont css
-import "@/styles/assets/iconfont/iconfont.scss";
+import '@/styles/assets/iconfont/iconfont.scss';
 // font css
-import "@/styles/assets/fonts/font.scss";
+import '@/styles/assets/fonts/font.scss';
 // element css
 import 'element-plus/dist/index.css';
 // custom element css
-import "@/styles/element.scss";
+import '@/styles/element.scss';
 // element plus
 import ElementPlus from 'element-plus';
 // element icons

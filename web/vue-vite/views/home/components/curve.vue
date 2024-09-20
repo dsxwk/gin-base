c3c8922161944483ba878732a000d9d9ba4bc095
dsx <25076778@qq.com>
2024-09-20 17:52:36 +0800

    调整首页翻译

--- web/vue-vite/views/home/components/curve.vue
+++ web/vue-vite/views/home/components/curve.vue
@@ -34,8 +34,8 @@ const option: ECOption = {
     formatter: (p: any) => {
       let dom = `<div style="width:100%; height: 70px !important; display:flex;flex-direction: column;justify-content: space-between;padding:10px;box-sizing: border-box;
       color:#fff; background: #6B9DFE;border-radius: 4px;font-size:14px; ">
-        <div style="display: flex; align-items: center;"> <div style="width:5px;height:5px;background:#ffffff;border-radius: 50%;margin-right:5px"></div>平台 :  ${p[0].name}</div>
-        <div style="display: flex;align-items: center;"><div style="width:5px;height:5px;background:#ffffff;border-radius: 50%;margin-right:5px"></div>数据量 :  ${p[0].value}</div>
+        <div style="display: flex; align-items: center;"> <div style="width:5px;height:5px;background:#ffffff;border-radius: 50%;margin-right:5px"></div>${funcs.lang('Platform')} :  ${p[0].name}</div>
+        <div style="display: flex;align-items: center;"><div style="width:5px;height:5px;background:#ffffff;border-radius: 50%;margin-right:5px"></div>${funcs.lang('Data Volume')} :  ${p[0].value}</div>
       </div>`;
       return dom;
     }

48fd083ca6eb0d0388c832bdd9ef859fba5f398e
dsx <25076778@qq.com>
2025-02-20 11:24:28 +0800

    调整配置和全局变量

--- common/global/global.go
+++ common/global/global.go
@@ -1,34 +1,25 @@
 package global
 
 import (
-	"errors"
 	"gin-base/config"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-	"gopkg.in/natefinch/lumberjack.v2"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
-	"log"
-	"os"
-	"time"
 )
 
+// 全局变量
 var (
-	DB     *gorm.DB // 数据库全局变量
-	Log    *zap.Logger
+	DB     = config.InitDB()
+	Log    = config.InitLogger()
 	Config = config.InitConfig()
 	Cache  = config.InitCache()
 )
 
-// 公共响应
+// Response 公共响应
 type Response struct {
 	Code    int64       `json:"code"`
 	Message string      `json:"msg"`
 	Data    interface{} `json:"data"`
 }
 
-// 公共分页数据返回
+// PageData 公共分页数据返回
 type PageData struct {
 	//  总条数
 	Total int64 `json:"total"`
@@ -42,94 +33,12 @@ type PageData struct {
 
 // 错误码
 const (
-	// 成功
+	// Success 成功
 	Success = 0
-	// 参数错误
+	// ArgsError 参数错误
 	ArgsError = 400
-	// 请求未授权
+	// Unauthorized 请求未授权
 	Unauthorized = 401
-	// 系统错误
+	// SystemError 系统错误
 	SystemError = 500
 )
-
-func init() {
-	InitLogger()
-
-	var err error
-	DB, err = gorm.Open(mysql.Open(Config.Mysql.Host), &gorm.Config{
-		Logger: logger.New(
-			log.New(os.Stdout, "\r\n", log.LstdFlags), // 设置log输出到控制台
-			logger.Config{
-				SlowThreshold: time.Millisecond * Config.Mysql.SlowQuerySeconds, //慢SQL阈值（单位：ms）
-				LogLevel:      logger.Info,
-				// IgnoreRecordNotFoundError: true, // 忽略Record Not Found 错误
-				Colorful: true, // 给log添加颜色
-			},
-		),
-	})
-	if err != nil {
-		Log.Error(err.Error(), zap.Error(err))
-		panic("数据库链接错误")
-	}
-	q, err := DB.DB()
-	q.SetMaxIdleConns(10)
-	q.SetMaxOpenConns(100)
-	q.SetConnMaxLifetime(59 * time.Second)
-
-	// 日志记录慢查询
-	LogSlowQuery(DB)
-}
-
-// 初始化日志
-func InitLogger() {
-	// 设置日志文件输出路径和文件年月日
-	logPath := "log/" + time.Now().Format("2006-01-02") + ".log"
-
-	// 配置 Lumberjack 日志轮转
-	lumberJackLogger := &lumberjack.Logger{
-		Filename:   logPath,
-		MaxSize:    Config.MaxSize,    // 单个日志文件大小（MB）
-		MaxBackups: Config.MaxBackups, // 最多保留的旧日志文件数
-		MaxAge:     Config.MaxDay,     // 保留的最大天数
-		Compress:   true,
-	}
-
-	// 创建一个Zap配置
-	encoderConfig := zap.NewProductionEncoderConfig()
-	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-
-	atomicLevel := zap.NewAtomicLevel()
-	atomicLevel.SetLevel(zap.InfoLevel)
-
-	core := zapcore.NewCore(
-		zapcore.NewJSONEncoder(encoderConfig),
-		zapcore.AddSync(lumberJackLogger),
-		atomicLevel,
-	)
-
-	Log = zap.New(core)
-}
-
-// 日志记录慢查询
-func LogSlowQuery(DB *gorm.DB) {
-	// 注册查询前回调
-	_ = DB.Callback().Query().Before("gorm:query").Register("slowquery:begin", func(db *gorm.DB) {
-		// 记录查询开始时间
-		db.InstanceSet("gorm:query_slowquery_start_time", time.Now())
-	})
-
-	// 注册查询后回调
-	_ = DB.Callback().Query().After("gorm:query").Register("slowquery:end", func(db *gorm.DB) {
-		// 获取查询开始时间
-		startTime, _ := db.InstanceGet("gorm:query_slowquery_start_time")
-		if startTime != nil {
-			elapsed := time.Since(startTime.(time.Time))
-			// 超过阈值则记录慢查询
-			if elapsed > time.Millisecond*Config.Mysql.SlowQuerySeconds {
-				sql := db.Statement.SQL.String()
-				// 记录慢查询的执行时间和查询语句
-				Log.Warn("执行慢查询："+elapsed.String(), zap.Error(errors.New("执行慢查询："+elapsed.String()+" sql："+sql)))
-			}
-		}
-	})
-}
--- config/config.go
+++ config/config.go
@@ -1,9 +1,16 @@
 package config
 
 import (
+	"errors"
 	"fmt"
 	"gin-base/helper"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 	"gopkg.in/yaml.v3"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"io/ioutil"
 	"log"
 	"os"
@@ -12,7 +19,7 @@ import (
 	"time"
 )
 
-// mysql
+// Mysql mysql
 type Mysql struct {
 	Host             string        `yaml:"host"`
 	Port             string        `yaml:"port"`
@@ -28,7 +35,7 @@ type Redis struct {
 	DB       int    `yaml:"db"`
 }
 
-// 日志
+// Log 日志
 type Log struct {
 	MaxSize    int `yaml:"max-size"`    // 单个日志文件大小（MB）
 	MaxBackups int `yaml:"max-backups"` // 最多保留的旧日志文件数
@@ -41,7 +48,7 @@ type Jwt struct {
 	Exp int64  `yaml:"exp"`
 }
 
-// 跨域
+// Cors 跨域
 type Cors struct {
 	AllowOrigin      string `yaml:"allow-origin"`
 	AllowHeaders     string `yaml:"allow-headers"`
@@ -50,13 +57,13 @@ type Cors struct {
 	AllowCredentials string `yaml:"allow-credentials"`
 }
 
-// 缓存
+// Cache 缓存
 type Cache struct {
 	Type  string `yaml:"type"`
 	Redis Redis
 }
 
-// 配置
+// Config 配置
 type Config struct {
 	Mysql
 	Log
@@ -65,8 +72,13 @@ type Config struct {
 	Cache
 }
 
-// 初始化配置
-func InitConfig() Config { // 初始化数据
+var (
+	config = InitConfig()
+	logs   = InitLogger()
+)
+
+// InitConfig 初始化配置
+func InitConfig() Config {
 	rootPath := GetRootPath()
 	file := filepath.Join(rootPath+"/config", "config.yaml")
 	yamlFile, err := ioutil.ReadFile(file)
@@ -80,11 +92,10 @@ func InitConfig() Config { // 初始化数据
 	return c
 }
 
-// 初始化缓存
+// InitCache 初始化缓存
 func InitCache() helper.CacheInterface {
 	var (
-		c      helper.CacheInterface
-		config = InitConfig()
+		c helper.CacheInterface
 	)
 
 	// 没有配置缓存类型,默认使用内存缓存
@@ -102,7 +113,69 @@ func InitCache() helper.CacheInterface {
 	return c
 }
 
-// 获取根目录
+// InitDB 初始化数据库
+func InitDB() *gorm.DB {
+	var (
+		err error
+		DB  *gorm.DB
+	)
+	DB, err = gorm.Open(mysql.Open(config.Mysql.Host), &gorm.Config{
+		Logger: logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags), // 设置log输出到控制台
+			logger.Config{
+				SlowThreshold: time.Millisecond * config.Mysql.SlowQuerySeconds, //慢SQL阈值（单位：ms）
+				LogLevel:      logger.Info,
+				// IgnoreRecordNotFoundError: true, // 忽略Record Not Found 错误
+				Colorful: true, // 给log添加颜色
+			},
+		),
+	})
+	if err != nil {
+		logs.Error(err.Error(), zap.Error(err))
+		panic("数据库链接错误")
+	}
+	q, err := DB.DB()
+	q.SetMaxIdleConns(10)
+	q.SetMaxOpenConns(100)
+	q.SetConnMaxLifetime(59 * time.Second)
+
+	// 日志记录慢查询
+	LogSlowQuery(DB)
+
+	return DB
+}
+
+// InitLogger 初始化日志
+func InitLogger() *zap.Logger {
+	// 设置日志文件输出路径和文件年月日
+	logPath := "log/" + time.Now().Format("2006-01-02") + ".log"
+
+	// 配置 Lumberjack 日志轮转
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   logPath,
+		MaxSize:    config.MaxSize,    // 单个日志文件大小（MB）
+		MaxBackups: config.MaxBackups, // 最多保留的旧日志文件数
+		MaxAge:     config.MaxDay,     // 保留的最大天数
+		Compress:   true,
+	}
+
+	// 创建一个Zap配置
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	atomicLevel := zap.NewAtomicLevel()
+	atomicLevel.SetLevel(zap.InfoLevel)
+
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig),
+		zapcore.AddSync(lumberJackLogger),
+		atomicLevel,
+	)
+
+	return zap.New(core)
+}
+
+// GetRootPath 获取根目录
 func GetRootPath() string {
 	pwd, err := os.Getwd()
 	if err != nil {
@@ -112,4 +185,28 @@ func GetRootPath() string {
 	adsPath := strings.Replace(pwd, "\\", "/", -1)
 
 	return adsPath
+}
+
+// LogSlowQuery 日志记录慢查询
+func LogSlowQuery(DB *gorm.DB) {
+	// 注册查询前回调
+	_ = DB.Callback().Query().Before("gorm:query").Register("slowquery:begin", func(db *gorm.DB) {
+		// 记录查询开始时间
+		db.InstanceSet("gorm:query_slowquery_start_time", time.Now())
+	})
+
+	// 注册查询后回调
+	_ = DB.Callback().Query().After("gorm:query").Register("slowquery:end", func(db *gorm.DB) {
+		// 获取查询开始时间
+		startTime, _ := db.InstanceGet("gorm:query_slowquery_start_time")
+		if startTime != nil {
+			elapsed := time.Since(startTime.(time.Time))
+			// 超过阈值则记录慢查询
+			if elapsed > time.Millisecond*config.Mysql.SlowQuerySeconds {
+				sql := db.Statement.SQL.String()
+				// 记录慢查询的执行时间和查询语句
+				logs.Warn("执行慢查询："+elapsed.String(), zap.Error(errors.New("执行慢查询："+elapsed.String()+" sql："+sql)))
+			}
+		}
+	})
 }

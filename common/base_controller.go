03d03c9555f6b07c62f396b634a8bd660ba539fc
dsx <25076778@qq.com>
2025-03-05 09:49:01 +0800

    调整全局错误码以及公共响应

--- common/base_controller.go
+++ common/base_controller.go
@@ -2,8 +2,6 @@ package common
 
 import (
 	"gin-base/common/global"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,26 +16,6 @@ func (s *BaseController) GetUserId(ctx *gin.Context) int64 {
 }
 
 // ApiResponse api统一返回
-func (s *BaseController) ApiResponse(ctx *gin.Context, code int64, message string, data interface{}) {
-	var response global.Response
-
-	if code != 0 {
-		response.Code = code
-		response.Message = "Error"
-	} else {
-		response.Message = "Success"
-	}
-
-	if message != "" {
-		response.Message = message
-	}
-
-	if data != nil {
-		response.Data = data
-	} else {
-		response.Data = []string{}
-	}
-
-	ctx.JSON(http.StatusOK, response)
-	ctx.Abort()
+func (s *BaseController) ApiResponse(ctx *gin.Context, errorCode global.ErrorCode, optionalParams ...interface{}) {
+	global.ApiResponse(ctx, errorCode, optionalParams...)
 }
--- common/global/global.go
+++ common/global/global.go
@@ -2,6 +2,8 @@ package global
 
 import (
 	"gin-base/config"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 // 全局变量
@@ -12,13 +14,6 @@ var (
 	Cache  = config.InitCache()
 	Redis  = config.InitRedis()
 )
-
-// Response 公共响应
-type Response struct {
-	Code    int64       `json:"code"`
-	Message string      `json:"msg"`
-	Data    interface{} `json:"data"`
-}
 
 // PageData 公共分页数据返回
 type PageData struct {
@@ -32,14 +27,57 @@ type PageData struct {
 	List interface{} `json:"list"`
 }
 
+// ErrorCode 错误码和提示信息
+type ErrorCode struct {
+	Code    int64
+	Message string
+}
+
+// Response 公共响应
+type Response struct {
+	Code    int64       `json:"code"`
+	Message string      `json:"msg"`
+	Data    interface{} `json:"data"`
+}
+
 // 错误码
-const (
-	// Success 成功
-	Success = 0
-	// ArgsError 参数错误
-	ArgsError = 400
-	// Unauthorized 请求未授权
-	Unauthorized = 401
-	// SystemError 系统错误
-	SystemError = 500
+var (
+	Success      = ErrorCode{Code: 0, Message: "Success"}
+	ArgsError    = ErrorCode{Code: 400, Message: "参数错误"}
+	Unauthorized = ErrorCode{Code: 401, Message: "请求未授权"}
+	SystemError  = ErrorCode{Code: 500, Message: "系统错误"}
 )
+
+// ApiResponse api统一返回
+func ApiResponse(ctx *gin.Context, errorCode ErrorCode, optionalParams ...interface{}) {
+	var response Response
+
+	response.Code = errorCode.Code
+
+	// 默认消息
+	response.Message = errorCode.Message
+
+	// 默认数据
+	response.Data = []string{}
+
+	// 解析可选参数,当第一个参数为字符串时默认为message否则为data
+	if len(optionalParams) > 0 {
+		if msg, ok := optionalParams[0].(string); ok {
+			response.Message = msg
+		} else {
+			response.Data = optionalParams[0]
+		}
+	}
+
+	if len(optionalParams) > 1 {
+		if msg, ok := optionalParams[0].(string); ok {
+			response.Message = msg
+		} else {
+			panic("参数错误,第一个参数message必须是字符串")
+		}
+		response.Data = optionalParams[1]
+	}
+
+	ctx.JSON(http.StatusOK, response)
+	ctx.Abort()
+}

68d7cd27ba432dcb16cbb3a99dfdc8b8779e6ec7
dsx <25076778@qq.com>
2025-03-14 12:06:42 +0800

    全局变量和函数优化调整

--- common/global/variable.go
+++ common/global/variable.go
@@ -5,7 +5,7 @@ import "gin-base/config"
 // 全局变量
 var (
 	FormatDate = "2006-01-02 15:04:05" // 全局日期格式
-	DB         = config.InitDB()       // 数据库
+	DB         = config.InitMysql()    // 数据库
 	Log        = config.InitLogger()   // 日志
 	Config     = config.InitConfig()   // 配置
 	Cache      = config.InitCache()    // 缓存

61a79aac6f34e5e2e66556224e12d54677fc389b
dsx <25076778@qq.com>
2025-03-18 10:31:55 +0800

    更新全局日志中间件

--- app/middleware/logger.go
+++ app/middleware/logger.go
@@ -2,14 +2,23 @@ package middleware
 
 import (
 	"gin-base/common/extend/context"
+	"gin-base/common/global"
+	"gin-base/config"
 	"github.com/gin-gonic/gin"
 )
 
 // LoggerMiddleware 全局日志中间件
 func LoggerMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		context.SetGinContext(c, "ginLogContext")
+		context.SetGinContext("GinLogger", c)
+		// 是否记录请求日志
+		if global.Config.Log.Access {
+			logFields := config.GetLogFields(nil)
+			global.Log.Info("Access Log", logFields...)
+		}
 
 		c.Next()
+
+		context.ClearGinContext("GinLogger")
 	}
 }

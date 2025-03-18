0ddb94478725736a0d10b6c8a5930e0cbb6b0080
dsx <25076778@qq.com>
2025-03-18 10:30:32 +0800

    新增错误码

--- common/global/errcode.go
+++ common/global/errcode.go
@@ -9,7 +9,9 @@ type ErrorCode struct {
 // 错误码
 var (
 	Success      = ErrorCode{Code: 0, Message: "Success"}
+	Redirect     = ErrorCode{Code: 301, Message: "数据已被重定向"}
 	ArgsError    = ErrorCode{Code: 400, Message: "参数错误"}
 	Unauthorized = ErrorCode{Code: 401, Message: "请求未授权"}
+	NotFound     = ErrorCode{Code: 404, Message: "资源未找到"}
 	SystemError  = ErrorCode{Code: 500, Message: "系统错误"}
 )

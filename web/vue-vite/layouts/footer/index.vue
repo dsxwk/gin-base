71f61f626cc3117d548fb42fcfc987c700a9dd03
dsx <25076778@qq.com>
2025-04-01 15:56:06 +0800

    移除web前端重构

--- web/vue-vite/layouts/footer/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="footer flx-center">
-    <a href="javascript:void(0);"> 2014-2024 © element-plus-admin by dsx </a>
-  </div>
-</template>
-<style lang="scss" scoped>
-@import './index.scss';
-</style>

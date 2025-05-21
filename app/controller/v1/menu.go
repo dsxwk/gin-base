6a1045f0cc1af2dccd5dc4274f5c10ded1e89773
dsx <25076778@qq.com>
2025-05-21 14:27:57 +0800

    新增菜单和菜单功能相关接口

---
+++ app/controller/v1/menu.go
@@ -0,0 +1,327 @@
+package v1
+
+import (
+	"gin-base/app/model"
+	"gin-base/app/service"
+	"gin-base/app/validate"
+	"gin-base/common"
+	"gin-base/common/global"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/copier"
+	"strconv"
+)
+
+type MenuController struct {
+	common.BaseController
+}
+
+// List 列表
+// @Router /api/v1/menu [get]
+func (s *MenuController) List(c *gin.Context) {
+	var (
+		menuService service.MenuService
+	)
+
+	data, err := menuService.List()
+	if err != nil {
+		global.Log.Error(err.Error())
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	s.ApiResponse(c, global.Success, data)
+}
+
+// Create 创建
+// @Router /api/v1/menu [post]
+func (s *MenuController) Create(c *gin.Context) {
+	var (
+		menuService  service.MenuService
+		menuValidate validate.MenuValidate
+		req          model.MenuQuery
+	)
+
+	err := c.ShouldBindQuery(&req)
+	if err != nil {
+		global.Log.Error(err.Error())
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	err = copier.Copy(&menuValidate, &req)
+	if err != nil {
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	// 验证
+	err = validate.GetMenuValidate(menuValidate, "create")
+	if err != nil {
+		s.ApiResponse(c, global.ArgsError, err.Error())
+		return
+	}
+
+	data, err := menuService.Create(req)
+	if err != nil {
+		global.Log.Error(err.Error())
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	s.ApiResponse(c, global.Success, data)
+}
+
+// Update 更新
+// @Router /api/v1/menu/{id} [put]
+func (s *MenuController) Update(c *gin.Context) {
+	var (
+		menuService  service.MenuService
+		menuValidate validate.MenuValidate
+		req          model.MenuQuery
+	)
+
+	idParam := c.Param("id")
+	if idParam == "" {
+		s.ApiResponse(c, global.ArgsError, "id参数必传")
+		return
+	}
+
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		s.ApiResponse(c, global.ArgsError, "id参数格式错误")
+		return
+	}
+
+	err = c.ShouldBind(&req)
+	if err != nil {
+		global.Log.Error(err.Error())
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	req.ID = id
+	menuValidate.ID = id
+	err = copier.Copy(&menuValidate, &req)
+	if err != nil {
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	// 验证
+	err = validate.GetMenuValidate(menuValidate, "update")
+	if err != nil {
+		s.ApiResponse(c, global.ArgsError, err.Error())
+		return
+	}
+
+	data, err := menuService.Update(req)
+	if err != nil {
+		global.Log.Error(err.Error())
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	s.ApiResponse(c, global.Success, "更新成功", data)
+}
+
+// Delete 删除
+// @Router /api/v1/menu/{id} [delete]
+func (s *MenuController) Delete(c *gin.Context) {
+	var (
+		menuService service.MenuService
+	)
+
+	idParam := c.Param("id")
+	if idParam == "" {
+		s.ApiResponse(c, global.ArgsError, "id参数必传")
+		return
+	}
+
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		s.ApiResponse(c, global.ArgsError, "id参数格式错误")
+		return
+	}
+
+	data, err := menuService.Delete(id)
+	if err != nil {
+		global.Log.Error(err.Error())
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	s.ApiResponse(c, global.Success, "删除成功", data)
+}
+
+// ActionList 功能列表
+// @Router /api/v1/menu/{menu_id}/action [get]
+func (s *MenuController) ActionList(c *gin.Context) {
+	var (
+		menuService service.MenuService
+	)
+
+	menuIDParam := c.Param("menu_id")
+	if menuIDParam == "" {
+		s.ApiResponse(c, global.ArgsError, "菜单id参数必传")
+		return
+	}
+
+	menuID, err := strconv.ParseInt(menuIDParam, 10, 64)
+	if err != nil {
+		s.ApiResponse(c, global.ArgsError, "id参数格式错误")
+		return
+	}
+
+	data, err := menuService.ActionList(menuID)
+	if err != nil {
+		global.Log.Error(err.Error())
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	s.ApiResponse(c, global.Success, data)
+}
+
+// ActionCreate 功能创建
+// @Router /api/v1/menu/{menu_id}/action [post]
+func (s *MenuController) ActionCreate(c *gin.Context) {
+	var (
+		menuService        service.MenuService
+		menuActionValidate validate.MenuActionValidate
+		req                model.MenuActionQuery
+	)
+
+	menuIDParam := c.Param("menu_id")
+	if menuIDParam == "" {
+		s.ApiResponse(c, global.ArgsError, "菜单id参数必传")
+		return
+	}
+
+	menuID, err := strconv.ParseInt(menuIDParam, 10, 64)
+	if err != nil {
+		s.ApiResponse(c, global.ArgsError, "菜单id参数格式错误")
+		return
+	}
+
+	err = c.ShouldBind(&req)
+	if err != nil {
+		global.Log.Error(err.Error())
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	req.MenuID = menuID
+	err = copier.Copy(&menuActionValidate, &req)
+	if err != nil {
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	// 验证
+	err = validate.GetMenuActionValidate(menuActionValidate, "create")
+	if err != nil {
+		s.ApiResponse(c, global.ArgsError, err.Error())
+		return
+	}
+
+	data, err := menuService.ActionCreate(req)
+	if err != nil {
+		global.Log.Error(err.Error())
+		s.ApiResponse(c, global.SystemError, err.Error())
+	}
+
+	s.ApiResponse(c, global.Success, "创建成功", data)
+}
+
+// ActionUpdate 功能更新
+// @Router /api/v1/menu/{menu_id}/action/{id} [put]
+func (s *MenuController) ActionUpdate(c *gin.Context) {
+	var (
+		menuService        service.MenuService
+		menuActionValidate validate.MenuActionValidate
+		req                model.MenuActionQuery
+	)
+
+	menuIDParam := c.Param("menu_id")
+	if menuIDParam == "" {
+		s.ApiResponse(c, global.ArgsError, "菜单id参数必传")
+		return
+	}
+
+	menuID, err := strconv.ParseInt(menuIDParam, 10, 64)
+	if err != nil {
+		s.ApiResponse(c, global.ArgsError, "菜单id参数格式错误")
+		return
+	}
+
+	IDParam := c.Param("id")
+	if IDParam == "" {
+		s.ApiResponse(c, global.ArgsError, "id参数必传")
+		return
+	}
+
+	ID, err := strconv.ParseInt(IDParam, 10, 64)
+	if err != nil {
+		s.ApiResponse(c, global.ArgsError, "id参数格式错误")
+		return
+	}
+
+	err = c.ShouldBind(&req)
+	if err != nil {
+		global.Log.Error(err.Error())
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	req.ID = ID
+	req.MenuID = menuID
+	err = copier.Copy(&menuActionValidate, &req)
+	if err != nil {
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	err = validate.GetMenuActionValidate(menuActionValidate, "update")
+	if err != nil {
+		s.ApiResponse(c, global.ArgsError, err.Error())
+		return
+	}
+
+	data, err := menuService.ActionUpdate(req)
+	if err != nil {
+		global.Log.Error(err.Error())
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	s.ApiResponse(c, global.Success, "更新成功", data)
+}
+
+// ActionDelete 功能删除
+// @Router /api/v1/menu/{menu_id}/action/{id} [delete]
+func (s *MenuController) ActionDelete(c *gin.Context) {
+	var (
+		menuService service.MenuService
+	)
+
+	iDParam := c.Param("id")
+	if iDParam == "" {
+		s.ApiResponse(c, global.ArgsError, "id参数必传")
+		return
+	}
+
+	ID, err := strconv.ParseInt(iDParam, 10, 64)
+	if err != nil {
+		s.ApiResponse(c, global.ArgsError, "id参数格式错误")
+		return
+	}
+
+	data, err := menuService.ActionDelete(ID, menuID)
+	if err != nil {
+		global.Log.Error(err.Error())
+		s.ApiResponse(c, global.SystemError, err.Error())
+		return
+	}
+
+	s.ApiResponse(c, global.Success, "删除成功", data)
+}
---
+++ app/service/menu.go
@@ -0,0 +1,181 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"gin-base/app/model"
+	"gin-base/common"
+	"gin-base/common/global"
+	"github.com/jinzhu/copier"
+	"gorm.io/gorm"
+)
+
+type MenuService struct {
+	common.BaseService
+}
+
+// List 列表
+// @return bool
+func (s *MenuService) List() (menus []model.MenuQuery, err error) {
+	var (
+		menuModel []model.Menu
+	)
+
+	err = global.DB.Preload("MenuAction", func(db *gorm.DB) *gorm.DB {
+		return db.Order("sort asc").Select("id,menu_id,type,name,is_link,sort,created_at,updated_at")
+	}).Order("sort asc").Find(&menuModel).Scan(&menus).Error
+
+	if err != nil {
+		return menus, err
+	}
+
+	for k, m := range menuModel {
+		menus[k].MenuAction = m.MenuAction
+	}
+
+	menus = s.MenuTree(menus, 0)
+
+	return menus, nil
+}
+
+// MenuTree 数据子集递归
+// @param menus []model.MenuQuery
+// @param pid int64
+// @return tree []model.MenuQuery
+func (s *MenuService) MenuTree(menus []model.MenuQuery, pid int64) (tree []model.MenuQuery) {
+	for _, menu := range menus {
+		if menu.Pid == pid {
+			children := s.MenuTree(menus, menu.ID)
+			menu.Children = children
+			tree = append(tree, menu)
+		}
+	}
+	return tree
+}
+
+// Create 创建
+// @param req model.MenuQuery
+// @return menuModel model.Menu, err error
+func (s *MenuService) Create(req model.MenuQuery) (menuModel model.Menu, err error) {
+	err = copier.Copy(&menuModel, &req)
+	if err != nil {
+		return menuModel, err
+	}
+
+	db := global.DB.Create(&menuModel)
+
+	err = db.Error
+	if err != nil {
+		return menuModel, err
+	}
+
+	return menuModel, nil
+}
+
+// Update 更新
+// @param req model.Menu
+// @return menuModel model.Menu, err error
+func (s *MenuService) Update(req model.MenuQuery) (menuModel model.Menu, err error) {
+	err = copier.Copy(&menuModel, &req)
+	if err != nil {
+		return menuModel, err
+	}
+
+	err = global.DB.Updates(&menuModel).Error
+	if err != nil {
+		return menuModel, err
+	}
+
+	return menuModel, nil
+}
+
+// Delete 删除
+// @param id int64
+// @return menuModel model.Menu, err error
+func (s *MenuService) Delete(id int64) (menuModel model.Menu, err error) {
+	err = global.DB.Delete(&menuModel, id).Error
+	if err != nil {
+		return menuModel, err
+	}
+
+	return menuModel, nil
+}
+
+// ActionList 功能列表
+// @param menuId int64
+// @return actionQuery []model.MenuActionQuery, err error
+func (s *MenuService) ActionList(menuId int64) (actionQuery []model.MenuActionQuery, err error) {
+	var (
+		menuActionModels []model.MenuAction
+	)
+
+	err = global.DB.
+		Where("menu_id = ?", menuId).
+		Order("sort asc").
+		Find(&menuActionModels).
+		Scan(&actionQuery).
+		Error
+	if err != nil {
+		return actionQuery, err
+	}
+
+	return actionQuery, nil
+}
+
+// ActionCreate 功能创建
+// @param req model.MenuActionQuery
+// @return menuActionModel model.MenuAction, err error
+func (s *MenuService) ActionCreate(req model.MenuActionQuery) (menuActionModel model.MenuAction, err error) {
+	err = copier.Copy(&menuActionModel, &req)
+	if err != nil {
+		return menuActionModel, err
+	}
+
+	db := global.DB.Create(&menuActionModel)
+
+	err = db.Error
+	if err != nil {
+		return menuActionModel, err
+	}
+
+	return menuActionModel, nil
+}
+
+// ActionUpdate 功能更新
+// @param req model.MenuActionQuery
+// @return menuActionModel model.MenuAction, err error
+func (s *MenuService) ActionUpdate(req model.MenuActionQuery) (menuActionModel model.MenuAction, err error) {
+	err = copier.Copy(&menuActionModel, &req)
+	if err != nil {
+		return menuActionModel, err
+	}
+
+	err = global.DB.Updates(&menuActionModel).Error
+	if err != nil {
+		return menuActionModel, err
+	}
+
+	return menuActionModel, nil
+}
+
+// ActionDelete 功能删除
+// @param id int64
+// @return menuActionModel model.MenuAction, err error
+func (s *MenuService) ActionDelete(id int64, menuID int64) (menuActionModel model.MenuAction, err error) {
+	if err = global.DB.Where("id = ?", id).First(&menuActionModel).Error; err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			return menuActionModel, err
+		}
+	}
+
+	if menuActionModel.MenuID != menuID {
+		return menuActionModel, fmt.Errorf("删除失败,该功能id【%d】不属于该菜单id【%d】", id, menuID)
+	}
+
+	err = global.DB.Delete(&menuActionModel, id).Error
+	if err != nil {
+		return menuActionModel, err
+	}
+
+	return menuActionModel, nil
+}

192978ee5e3914d2290160dd895a0db5627ed759
dsx <25076778@qq.com>
2025-05-23 16:51:27 +0800

    新增菜单元数据注释

--- web/static/menu/index.js
+++ web/static/menu/index.js
@@ -1,3 +1,18 @@
+/**
+ * 建议：路由 path 路径与文件夹名称相同，找文件可浏览器地址找，方便定位文件位置
+ *
+ * 路由meta对象参数说明
+ * meta: {
+ *      title:          菜单栏及 tagsView 栏、菜单搜索名称（国际化）
+ *      isLink：        是否超链接菜单，开启外链条件，`1、isLink: 链接地址不为空 2、isIframe:false`
+ *      isHide：        是否隐藏此路由
+ *      isKeepAlive：   是否缓存组件状态
+ *      isAffix：       是否固定在 tagsView 栏上
+ *      isIframe：      是否内嵌窗口，开启条件，`1、isIframe:true 2、isLink：链接地址不为空`
+ *      roles：         当前路由权限标识，取角色管理。控制路由显示、隐藏。超级管理员：admin 普通角色：common
+ *      icon：          菜单、tagsView 图标，阿里：加 `iconfont xxx`，fontawesome：加 `fa xxx`
+ * }
+ */
 export const menuJson = {
     "code": 0,
     "type": "adminMenu",
@@ -30,7 +45,7 @@ export const menuJson = {
             "pid": 0,
             "path": "/system",
             "name": "system",
-            "component": "layout/routerView/parent",
+            "component": "layouts/routerView/parent",
             "redirect": "/system/menu",
             "meta": {
                 "title": "message.router.system",

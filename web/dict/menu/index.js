a5cfbc3e4959d99949344923677c14a446aab68d
dsx <25076778@qq.com>
2025-05-23 16:50:36 +0800

    新增菜单字典

---
+++ web/dict/menu/index.js
@@ -0,0 +1,9 @@
+export const isLinkDict = [
+    { label: '是', value: true },
+    { label: '否', value: false }
+];
+
+export const isHideDict = [
+    { label: '是', value: true },
+    { label: '否', value: false }
+];

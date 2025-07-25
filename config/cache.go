9615589ac41cc622da2faf29ab3c9b4544ceffc9
dsx <25076778@qq.com>
2025-07-25 17:23:44 +0800

    调整缓存type配置为driver

--- config/cache.go
+++ config/cache.go
@@ -9,8 +9,8 @@ import (
 
 // Cache 缓存
 type Cache struct {
-	Type  string `yaml:"type"`
-	Redis Redis
+	Driver string `yaml:"type"`
+	Redis  Redis
 }
 
 // InitCache 初始化缓存
@@ -20,18 +20,18 @@ func InitCache(config *Config) cache.InterfaceCache {
 	)
 
 	// 没有配置缓存类型,默认使用内存缓存
-	if config.Cache.Type == "memory" || config.Cache.Type == "" {
+	if config.Cache.Driver == "memory" || config.Cache.Driver == "" {
 		c = cache.NewMemoryCache(5*time.Minute, 10*time.Minute)
-	} else if config.Cache.Type == "disk" {
+	} else if config.Cache.Driver == "disk" {
 		c, _ = cache.NewDiskCache("storage/cache")
-	} else if config.Cache.Type == "redis" {
+	} else if config.Cache.Driver == "redis" {
 		// 使用 Redis 缓存
 		c = InitRedis(config)
 	} else {
-		log.Fatalf("Unsupported cache type: %s", config.Cache.Type)
+		log.Fatalf("Unsupported cache driver: %s", config.Cache.Driver)
 	}
 
-	fmt.Println("Cache initialized with type:", config.Cache.Type)
+	fmt.Println("Cache initialized with driver:", config.Cache.Driver)
 
 	return c
 }
